ee7e00121b63ae5093a2f834f9757b6783b999de
betroelectro <73169048+betroelectro@users.noreply.github.com>
2023-04-08 10:07:45 +0200

    Update on2.py

--- Beamer/on2.py
+++ Beamer/on2.py
@@ -7,7 +7,7 @@ an = '"relay_state":1'
 if aus in status:
     os.system('/var/www/tplink-smartplug/tplink_smartplug.py -t 192.168.178.158 -c on')
     time.sleep(5)
-    os.system('echo "* 0 IR 001\r" >/dev/ttyS0')
+    os.system('echo -e "* 0 IR 001\r" >/dev/ttyS0')
 elif an in status:
-    os.system('echo "* 0 IR 002\r" >/dev/ttyS0')
+    os.system('echo -e "* 0 IR 002\r" >/dev/ttyS0')
     time.sleep(60)
