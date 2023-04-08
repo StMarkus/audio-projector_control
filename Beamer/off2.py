fb8851a72a41929cddfdb480c0964875399fd7ba
betroelectro <73169048+betroelectro@users.noreply.github.com>
2023-04-08 10:08:12 +0200

    Update off2.py

--- Beamer/off2.py
+++ Beamer/off2.py
@@ -1,6 +1,6 @@
 import os
 import time
 
-os.system('echo "* 0 IR 002\r" >/dev/ttyS0')
+os.system('echo -e "* 0 IR 002\r" >/dev/ttyS0')
 time.sleep(60)
 os.system('/var/www/tplink-smartplug/tplink_smartplug.py -t 192.168.178.158 -c off')
